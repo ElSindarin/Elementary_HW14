55d75dcb6bcd9daad2ca35ba3304e3d4b8e0eb9a
ElSindarin <53188567+ElSindarin@users.noreply.github.com>
2019-09-14 23:29:35 +0300

    Optimized Main algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,25 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        DataCollection dataCollection1 = new DataCollection(new RegularSortStrategy(), new ArrayList<>());
-        dataCollection1.addData(new Element(126513));
-        dataCollection1.addData(new Element("ololololo"));
-        dataCollection1.addData(new Element(126513D));
-        dataCollection1.addData(new Element(1F));
-        DataCollection.showCollection(dataCollection1);
-        dataCollection1.showStrategy();
-        DataCollection dataCollection2 = new DataCollection(new ReversedSortStrategy(), new ArrayList<>());
-        dataCollection2.addData(new Element(126513));
-        dataCollection2.addData(new Element("ololololo"));
-        dataCollection2.addData(new Element(126513D));
-        dataCollection2.addData(new Element(1F));
-        DataCollection.showCollection(dataCollection2);
-        dataCollection2.showStrategy();
-        DataCollection dataCollection3 = new DataCollection(new NoSortStrategy(), new ArrayList<>());
-        dataCollection3.addData(new Element(126513));
-        dataCollection3.addData(new Element("ololololo"));
-        dataCollection3.addData(new Element(126513D));
-        dataCollection3.addData(new Element(1F));
-        DataCollection.showCollection(dataCollection3);
-        dataCollection3.showStrategy();
+        DataCollection dataCollection = new DataCollection(new RegularSortStrategy(), new ArrayList<>());
+
+        demonstrateStrategyPattern(dataCollection, new RegularSortStrategy());
+        demonstrateStrategyPattern(dataCollection, new ReversedSortStrategy());
+        demonstrateStrategyPattern(dataCollection, new NoSortStrategy());
+    }
+
+    public static void demonstrateStrategyPattern (DataCollection dataCollection, Strategy strategy) {
+        dataCollection.showStrategy();
+        dataCollection.setStrategy(strategy);
+        dataCollection.addData(new Element(126513));
+        dataCollection.addData(new Element("ololololo"));
+        dataCollection.addData(new Element(126513D));
+        dataCollection.addData(new Element(1F));
+        DataCollection.showCollection(dataCollection);
+        dataCollection.getCollection().clear();
     }
 }
